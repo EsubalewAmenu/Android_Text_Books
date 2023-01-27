592a10916a0b2c84e111b89fcad13f671fb500e2
Esubalew Desktop <esubalew.a2009@gmail.com>
2023-01-27 12:21:53 +0300

    basic FloatingActionButton for interactions

--- app/src/main/java/com/herma/apps/textbooks/ReadActivity.java
+++ app/src/main/java/com/herma/apps/textbooks/ReadActivity.java
@@ -46,6 +46,7 @@ import com.google.android.gms.ads.rewarded.RewardItem;
 import com.google.android.gms.ads.rewarded.RewardedAd;
 import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 import com.google.android.gms.ads.rewarded.ServerSideVerificationOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.herma.apps.textbooks.common.Commons;
 import com.herma.apps.textbooks.ui.about.About_us;
 
@@ -79,6 +80,15 @@ public class ReadActivity extends AppCompatActivity {
     TextView txtTimerValue;
     ImageButton btnGiftReward;
     long reward_p_id, reward_minutes;
+
+
+    FloatingActionButton mAddFab, mAddAlarmFab, mAddPersonFab;
+
+    // These are taken to make visible and invisible along with FABs
+    TextView addAlarmActionText, addPersonActionText;
+
+    // to check whether sub FAB buttons are visible or not.
+    Boolean isAllFabsVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -183,9 +193,72 @@ public class ReadActivity extends AppCompatActivity {
 
         /////////////
 
-
+        commentRelateds();
     }
+public void commentRelateds(){
 
+    // Register all the FABs with their IDs This FAB button is the Parent
+    mAddFab = findViewById(R.id.add_fab);
+
+    // FAB button
+    mAddAlarmFab = findViewById(R.id.add_alarm_fab);
+    mAddPersonFab = findViewById(R.id.add_person_fab);
+
+    // Also register the action name text, of all the FABs.
+    addAlarmActionText = findViewById(R.id.add_alarm_action_text);
+    addPersonActionText = findViewById(R.id.add_person_action_text);
+
+    // Now set all the FABs and all the action name texts as GONE
+    mAddAlarmFab.setVisibility(View.GONE);
+    mAddPersonFab.setVisibility(View.GONE);
+    addAlarmActionText.setVisibility(View.GONE);
+    addPersonActionText.setVisibility(View.GONE);
+
+    // make the boolean variable as false, as all the
+    // action name texts and all the sub FABs are invisible
+    isAllFabsVisible = false;
+
+    // We will make all the FABs and action name texts
+    // visible only when Parent FAB button is clicked So
+    // we have to handle the Parent FAB button first, by
+    // using setOnClickListener you can see below
+    mAddFab.setOnClickListener(view -> {
+        if (!isAllFabsVisible) {
+            // when isAllFabsVisible becomes true make all
+            // the action name texts and FABs VISIBLE
+            mAddAlarmFab.show();
+            mAddPersonFab.show();
+            addAlarmActionText.setVisibility(View.VISIBLE);
+            addPersonActionText.setVisibility(View.VISIBLE);
+
+            // make the boolean variable true as we
+            // have set the sub FABs visibility to GONE
+            isAllFabsVisible = true;
+        } else {
+            // when isAllFabsVisible becomes true make
+            // all the action name texts and FABs GONE.
+            mAddAlarmFab.hide();
+            mAddPersonFab.hide();
+            addAlarmActionText.setVisibility(View.GONE);
+            addPersonActionText.setVisibility(View.GONE);
+
+            // make the boolean variable false as we
+            // have set the sub FABs visibility to GONE
+            isAllFabsVisible = false;
+        }
+    });
+    // below is the sample action to handle add person FAB. Here it shows simple Toast msg.
+    // The Toast will be shown only when they are visible and only when user clicks on them
+    mAddPersonFab.setOnClickListener(
+            view -> Toast.makeText(ReadActivity.this, "Person Added", Toast.LENGTH_SHORT
+            ).show());
+
+    // below is the sample action to handle add alarm FAB. Here it shows simple Toast msg
+    // The Toast will be shown only when they are visible and only when user clicks on them
+    mAddAlarmFab.setOnClickListener(
+            view -> Toast.makeText(ReadActivity.this, "Alarm Added", Toast.LENGTH_SHORT
+            ).show());
+}
     private void requestPhoneNumber() {
 
 
