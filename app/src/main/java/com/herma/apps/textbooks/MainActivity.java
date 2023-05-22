2c434f82637a0699913d4be422454c32eed1c130
Esubalew <esubalew.a2009@gmail.com>
2023-05-22 11:25:21 +0300

    experts way app link ad added

--- app/src/main/java/com/herma/apps/textbooks/MainActivity.java
+++ app/src/main/java/com/herma/apps/textbooks/MainActivity.java
@@ -591,6 +591,9 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_college_books) {
             openUrl("https://play.google.com/store/apps/details?id=com.herma.apps.collegebooks&hl=en_US&gl=US");
             return true;
+        } else if (id == R.id.nav_expertsway) {
+            openUrl("https://play.google.com/store/apps/details?id=com.herma.apps.expertsway");
+            return true;
 //        } else if (id == R.id.nav_add_books) {
 //            openUrl("https://docs.google.com/forms/d/e/1FAIpQLSfCJtHIIZYY0CJe0V0W4GLkgr1407qMG3RwOs2KTqiIxt53ig/viewform?usp=sf_link");
         } else if (id == R.id.nav_rate) {
